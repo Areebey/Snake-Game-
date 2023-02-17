b3372f1258a7eafa6783350a49f9c826bfe84970
Areebey <areebrafiq125@gmail.com>
2023-02-18 00:43:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// A simple Snake Game 
+
+function init(){
+    let canvas=document.getElementById("mycanvas");
+
+    W = H = canvas.width = canvas.height = 1000;
+    pen=canvas.getContext('2d');
+    
+
+}
+
+function draw(){
+
+}
+
+function update(){
+
+}
+
+function gameLoop(){
+
+}
